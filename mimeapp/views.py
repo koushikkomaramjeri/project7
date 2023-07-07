bed42cf7fbd207610059a6cd8104b09e8894405d
saikondepati35 <saikondepati35@gmail.com>
2023-07-07 17:51:18 +0530

    Initial commit

---
+++ mimeapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views import  View
+from django.http import HttpResponse
+data="""<table><tr><th>empno</th><th>ename</th><th>esl</th></tr>
+<tr><td>7788</td><td>scott</td><td>3000.00</td></tr>
+<tr><td>7902</td><td>blake</td><td>4000.00</td></tr>
+<tr><td>7369</td><td>miller</td><td>3500.00</td></tr></table>"""
+class HtmlView(View):
+    def get(self,request):
+        res=HttpResponse(data,content_type="text/html")
+        return res
+class XmlView(View):
+    def get(self,request):
+        res=HttpResponse(data,content_type="application/xml")
+        return res
+class ExcelView(View):
+    def get(self,request):
+        res=HttpResponse(data,content_type="application/vnd.ms-excel")
+        return res
